18b0cb12fec5d2b52bd8fe1602470c47d871ea9f
Albert Smith <altiar.albert@yahoo.com>
2017-03-23 22:46:19 +0800

    初步完成水波型加载

--- app/src/main/java/net/hivebc/waterprogress/MainActivity.java
+++ app/src/main/java/net/hivebc/waterprogress/MainActivity.java
@@ -1,13 +1,38 @@
 package net.hivebc.waterprogress;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import net.hivebc.waterprogress.view.WaterProgress;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    WaterProgress waterProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = (SeekBar) findViewById(R.id.seek);
+        waterProgress = (WaterProgress) findViewById(R.id.progress);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int progress = seekBar.getProgress();
+                waterProgress.setProgress(progress);
+                Toast.makeText(getBaseContext(), "P: " + progress, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/net/hivebc/waterprogress/view/WaterProgress.java
+++ app/src/main/java/net/hivebc/waterprogress/view/WaterProgress.java
@@ -1,69 +1,181 @@
 package net.hivebc.waterprogress.view;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.graphics.Region;
+import android.os.Build;
+import android.support.annotation.FloatRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import net.hivebc.waterprogress.R;
+
 
 /**
  * Created by albert on 17-3-13.
  */
 
 public class WaterProgress extends View {
+    public static final int defaultDiam = 200;
+    public static final int MIN_DIAM_SIZE = 20;
+    private final int maxProgress = 100;
+    Paint paint = new Paint();
+    Path path = new Path();
+    private int progress;
+    private boolean showProgress;
+    private float speed;
+    private float wave;
+    private float diam;
+    private float radius;
+    private float xCircle;
+    private float yCircle;
+    private int color;
+    private int backgroundColor;
+    private int offset = 1;
+    private int strokSize = dp2px(4);
+    private float textSize = dp2px(20);
+
     public WaterProgress(Context context) {
         super(context);
     }
 
     public WaterProgress(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public WaterProgress(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.WaterProgress, defStyleAttr, 0);
+        init(a);
     }
 
-    private int progress;
-    private int maxProgress;
-    Paint paint = new Paint();
-    Path path = new Path();
-    private int count = 1;
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public WaterProgress(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.WaterProgress, defStyleAttr, defStyleRes);
+        init(a);
+    }
+
+    private void init(TypedArray a) {
+        speed = a.getFloat(R.styleable.WaterProgress_speed, 1f);
+        wave = a.getFloat(R.styleable.WaterProgress_wave, 1f);
+        showProgress = a.getBoolean(R.styleable.WaterProgress_showProgress, true);
+        color = a.getColor(R.styleable.WaterProgress_color, Color.CYAN);
+        backgroundColor = a.getColor(R.styleable.WaterProgress_backgroundColor, Color.WHITE);
+        float diam = a.getDimension(R.styleable.WaterProgress_diam, dp2px(defaultDiam));
+        setDiam(diam);
+        a.recycle();
+        radius = diam / 2f;
+        xCircle = radius + strokSize;
+        yCircle = radius + strokSize;
+    }
+
+    public synchronized void setSpeed(@FloatRange(from = 0.0, to = 1f) float speed) {
+        this.speed = speed;
+    }
+
+    public synchronized void setWave(@FloatRange(from = 0.0, to = 1f) float wave) {
+        this.wave = wave;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension((int) diam + strokSize * 2, (int) diam + strokSize * 2);
+    }
+
+    public void setDiam(float diam) {
+        if (diam < dp2px(MIN_DIAM_SIZE))
+            diam = dp2px(MIN_DIAM_SIZE);
+        this.diam = diam;
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.CYAN);
-        paint.setStrokeWidth(4);
-        canvas.drawColor(Color.GRAY);
-        path.reset();
-        path.addCircle(150,100,100, Path.Direction.CCW);
-        canvas.clipPath(path);
-        canvas.drawCircle(150, 100, 104, paint);
-        canvas.save();
-        canvas.translate(count,0);
-        path.reset();
-        path.moveTo(0, 100);
-        path.quadTo(50, 50, 100, 100);
-        path.quadTo(160 , 150, 250 , 100);
-        canvas.drawPath(path, paint);
-        canvas.restore();
+        drawCircle(canvas);
+        drawWave(canvas);
+        if (showProgress) {
+            drawProgress(canvas);
+        }
         postInvalidateDelayed(5);
-        if (count < 50) count++;
-        else count = 1;
+        if (offset < diam) {
+            offset += 10 * speed;
+        } else {
+            offset = 0;
+        }
     }
 
-    public void setProgress(int progress) {
+    private void drawProgress(Canvas canvas) {
+        paint.reset();
+        paint.setColor(Color.BLACK);
+        paint.setAntiAlias(true);
+        paint.setTextSize(textSize);
+        paint.setTextAlign(Paint.Align.CENTER);
+        String text = (int) getPercent(progress) + "%";
+        Paint.FontMetrics fm = paint.getFontMetrics();
+        canvas.drawText(text, xCircle, yCircle - fm.descent + Math.abs(fm.bottom - fm.top) / 2, paint);
+    }
+
+    private void drawWave(Canvas canvas) {
+        path.reset();
+        path.addCircle(xCircle, yCircle, radius, Path.Direction.CCW);
+        canvas.clipPath(path);
+        buildWave(path);
+        paint.setAntiAlias(true);
+        paint.setColor(color);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawPath(path, paint);
+    }
+
+    private void drawCircle(Canvas canvas) {
+        paint.reset();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(color);
+        paint.setStrokeWidth(strokSize);
+        canvas.drawColor(backgroundColor);
+        canvas.drawCircle(xCircle, yCircle, radius, paint);
+    }
+
+    public synchronized void setProgress(int progress) {
         if (progress < 0)
             return;
         if (progress > maxProgress)
             progress = maxProgress;
-        invalidate();
+        this.progress = progress;
+        postInvalidate();
     }
 
+    private void buildWave(@NonNull Path path) {
+        // wave path
+        path.reset();
+        path.moveTo(-diam + offset, diam + strokSize * 2);
+        float levelHeight = getLevelHeight(progress);
+        float quarterDiameter = diam / 4f;
+        path.lineTo(-diam + offset, levelHeight);
+        path.quadTo(-diam * 3f / 4 + offset, levelHeight + quarterDiameter * wave, -diam / 2f + offset, levelHeight);
+        path.quadTo(-quarterDiameter + offset, levelHeight - quarterDiameter * wave, offset, levelHeight);
+        path.quadTo(quarterDiameter + offset, levelHeight + quarterDiameter * wave, diam / 2f + offset, levelHeight);
+        path.quadTo(diam * 3f / 4 + offset, levelHeight - quarterDiameter * wave, diam + offset, levelHeight);
+        path.lineTo(diam, diam + strokSize * 2);
+        path.close();
+    }
+
+    private float getLevelHeight(int progress) {
+        return diam * (1 - getPercent(progress) / 100);
+    }
+
+    private float getPercent(int progress) {
+        return progress * 1f / maxProgress * 100;
+    }
+
+    public int dp2px(int value) {
+        final float scale = getResources().getDisplayMetrics().densityDpi;
+        return (int) (value * (scale / 160) + 0.5f);
+    }
 }
